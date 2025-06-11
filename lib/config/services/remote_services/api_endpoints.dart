48b7edb5792c26ee05c550d966a7f461ab6e219c
srishtitater <srishtitater@gmail.com>
2025-06-11 08:41:53 +0545

    New Commit

---
+++ lib/config/services/remote_services/api_endpoints.dart
@@ -0,0 +1,12 @@
+
+class ApiEndPoints {
+
+
+  static String baseUrl = 'https://majorproject-khhl.onrender.com/api/';
+
+
+  static String userLogin = 'user/login';
+  static String userRegistration = 'user/register';
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sewa_mitra/config/local_db/hive_setup.dart';
 import 'package:sewa_mitra/feature/auth/view/login_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await HiveSetup.initHive;
+  runApp(const ProviderScope(child: MyApp()));
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
