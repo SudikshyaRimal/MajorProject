3d06fc30ea074a0d3515c2b24dd349b69450160a
binisha12 <118426837+binisha12@users.noreply.github.com>
2025-06-14 16:55:15 +0545

    Create categoryRoutes.js

---
+++ backend/routes/categoryRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  createCategory,
+  getAllCategories,
+} from "../Controllers/categoryController.js";
+
+const router = express.Router();
+
+// ✅ CREATE CATEGORY — One-time, for Admin to create fixed ones
+router.post("/create", createCategory);
+
+// ✅ GET ALL CATEGORIES — For frontend to show category buttons
+router.get("/all", getAllCategories);
+
+export default router;
