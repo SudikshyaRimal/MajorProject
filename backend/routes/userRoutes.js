83e5baa29c217a2c1d6f298784e11f853fe8183a
SudikshyaRimal <103429520+SudikshyaRimal@users.noreply.github.com>
2025-06-21 21:01:23 +0545

    Add files via upload

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,17 +5,22 @@ import {
   logout,
   sendResetOtp,
   verifyResetOtp,
-  resetPassword
+  resetPassword,
+  editProfile,
+  changePassword
 } from '../Controllers/userController.js';
 import userAuth from "../middleware/userAuth.js"; 
 
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.post('/register', userAuth,registerUser);
+router.post('/login', userAuth,loginUser);
 router.post('/logout', logout);
-router.post('/send-reset-otp', sendResetOtp);
-router.post('/verify-reset-otp', verifyResetOtp);
-router.post('/reset-password', resetPassword);
+router.post('/send-reset-otp', userAuth,sendResetOtp);
+router.post('/verify-reset-otp',userAuth, verifyResetOtp);
+router.post('/reset-password', userAuth,resetPassword);
+router.post('/edit-profile', userAuth,editProfile);
+router.post('/changePassword', userAuth,changePassword);
+
 
 export default router;
